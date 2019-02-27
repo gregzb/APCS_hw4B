2fb590e53b0923624fbb8014949d16f9eb1a5b8c
Greg Zborovsky <greg.zbo3@gmail.com>
2019-02-27 12:37:59 -0500

    Save instance of HelloWorld to a variable

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
     }
 
     public static void main(String[] args) {
-        System.out.println(new HelloWorld().greet());
+	HelloWorld myWorld = new HelloWorld();
+        System.out.println(myWorld.greet() + "\n" + myWorld.greet());
     }
 }
